d20e65f9704205559033743c3a4c7defebf42f72
최정균 <wjdrbs966@naver.com>
2019-12-30 15:45:10 +0900

    오늘도 자바 공부중

---
+++ AbstractPractice/Cat.java
@@ -0,0 +1,9 @@
+package AbstractPractice;
+
+public class Cat extends Animal {
+    public int leg;
+
+    public void eat(){
+        System.out.println("고양이는 냐옹냐옹");
+    }
+}
---
+++ AbstractPractice/dog.java
@@ -0,0 +1,11 @@
+package AbstractPractice;
+
+public class dog extends Animal{
+    public int leg;
+
+    public void eat(){
+        System.out.println("강아지는 냠냠냠");
+    }
+
+
+}
