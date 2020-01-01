d64d78749cdeb048d02c29e85bfaf79b09f49e9c
최정균 <wjdrbs966@naver.com>
2020-01-02 00:01:13 +0900

    간단하게 문법 테스트 중 입니다

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    public static void main(String[] args){
+        Animal cat,dog;  // 객체
+
+        cat = new Animal();
+        //cat은 Animal 클래스의 인스턴스(객체를 메모리에 할당)
+        dog = new Animal();
+        //dog은 Animal 클래스의 인스턴스(객체를 메모리에 할당)
+
+
+    }
+}
---
+++ Practice.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Practice {
+    public static void main(String[] args){
+        ArrayList list = new ArrayList();
+
+        list.add(3); list.add(5); list.add(7);
+
+    }
+}
