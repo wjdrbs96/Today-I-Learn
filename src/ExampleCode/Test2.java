7934241dcf720b41689d8e30ce98ccb52cadc147
Gyunny <wjdrbs966@naver.com>
2020-11-10 23:54:29 +0900

    Create: EffectiveJava item6 add

---
+++ src/ExampleCode/Test2.java
@@ -0,0 +1,12 @@
+package ExampleCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> hm = new HashMap<>();
+
+    }
+}
