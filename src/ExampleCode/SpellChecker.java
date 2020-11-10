5dff82744862a8849b2a93c67795382afaca7eea
Gyunny <wjdrbs966@naver.com>
2020-11-10 18:34:45 +0900

    Create: Java type add

--- src/ExampleCode/SpellChecker.java
+++ src/ExampleCode/SpellChecker.java
@@ -1,5 +1,13 @@
 package ExampleCode;
 
 public class SpellChecker {
+    public static void main(String[] args) {
+        int a = 3;
+        System.out.println(System.identityHashCode(a));
+        a = 2;
+        System.out.println(System.identityHashCode(a));
+        a = 3;
+        System.out.println(System.identityHashCode(a));
 
+    }
 }
