aab43af1a8ba7c108e97bb3cbf4fbe4c0ffa88b2
Gyunny <wjdrbs966@naver.com>
2021-06-22 10:35:54 +0900

    Update

--- src/ExampleCode/Test.java
+++ src/ExampleCode/Test.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Test {
+public class    Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
