ad91dcf0b7e772413ecff712519f46ca6cf93658
Gyunny <wjdrbs966@naver.com>
2021-08-07 01:43:22 +0900

    Update

--- src/ExampleCode/DateFormatEx1.java
+++ src/ExampleCode/DateFormatEx1.java
@@ -2,9 +2,6 @@ package ExampleCode;
 
 public class DateFormatEx1 {
     public static void main(String[] args) {
-        String a = "abc";
-        String b = "abc";
 
-        System.out.println(a == b);
     }
 }
