e1a1674454807345f8b3f9b668ca3870e021390d
Gyunny <wjdrbs966@naver.com>
2021-08-27 16:32:48 +0900

    Kotlin

--- src/ExampleCode/Hi.java
+++ src/ExampleCode/Hi.java
@@ -1,7 +1,13 @@
 package ExampleCode;
 
+import java.util.Arrays;
+
 /**
  * created by Gyunny 2021/08/17
  */
 public class Hi {
+    public static void main(String[] args) {
+        String[] split = "12.345-6.A".split(".");
+        System.out.println(Arrays.toString(split)); // []
+    }
 }
