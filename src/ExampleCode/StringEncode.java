7ce4b18bccfadb98b026b1dc959fd62ecabe61f7
wjdrbd96 <wjdrbs966@naver.com>
2024-03-26 00:01:29 +0900

    String encode

--- src/ExampleCode/StringEncode.java
+++ src/ExampleCode/StringEncode.java
@@ -1,6 +1,8 @@
 package ExampleCode;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class StringEncode {
@@ -13,11 +15,19 @@ public class StringEncode {
         String result1 = new String(isoBytes, StandardCharsets.ISO_8859_1);
         String result2 = new String(utf8Bytes, StandardCharsets.UTF_8);
         String result3 = new String(isoBytes, StandardCharsets.UTF_8);
+        String result4 = new String(isoBytes);
+
+        String encode = URLEncoder.encode(korean, StandardCharsets.UTF_8);
+        System.out.println("encode: " + encode);
+        String decode = URLDecoder.decode(encode, StandardCharsets.ISO_8859_1);
+        System.out.println("decode: " + decode);
+
+        String name = new String(decode.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        System.out.println("name: " + name);
 
         System.out.println("result1: " + result1);
         System.out.println("result2: " + result2);
         System.out.println("result3: " + result3);
-
-
+        System.out.println("result4: " + result4);
     }
 }
