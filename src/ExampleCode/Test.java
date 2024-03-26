8da4560477bbe47a25fd6b59400dc71a2f357d59
wjdrbd96 <wjdrbs966@naver.com>
2024-03-26 20:22:55 +0900

    MySQL InnoDB Index

--- src/ExampleCode/Test.java
+++ src/ExampleCode/Test.java
@@ -11,6 +11,9 @@ import java.io.UnsupportedEncodingException;
  */
 public class Test {
     public static void main(String[] args) throws UnsupportedEncodingException {
-
+        String korean = "한글";
+        byte[] bytes = korean.getBytes();
+        String string = new String(bytes);
+        System.out.println(string);
     }
 }
