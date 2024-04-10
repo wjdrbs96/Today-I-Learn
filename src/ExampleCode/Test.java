e81b9a5a253860941cb480c08c1a035eadec9a77
wjdrbd96 <wjdrbs966@naver.com>
2024-04-11 00:17:15 +0900

    test

--- src/ExampleCode/Test.java
+++ src/ExampleCode/Test.java
@@ -1,6 +1,10 @@
 package ExampleCode;
 
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Test
@@ -11,9 +15,27 @@ import java.io.UnsupportedEncodingException;
  */
 public class Test {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String korean = "한글";
-        byte[] bytes = korean.getBytes();
-        String string = new String(bytes);
-        System.out.println(string);
+        System.out.println("start");
+        for (int i = 0; i < 50; ++i) {
+            CompletableFuture.supplyAsync(
+                    () -> {
+                        System.out.println("Thread: " + Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+
+                        }
+                        return null;
+                    },
+                    SELECT_THREAD_POOL
+            );
+        }
+
+        System.out.println("end");
     }
+
+    public static final ThreadPoolExecutor SELECT_THREAD_POOL = new ThreadPoolExecutor(
+            20, 20,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>());
 }
